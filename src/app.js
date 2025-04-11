4e089d71822a813b7dc73020b8c27386dbb160ec
RahulSha7 <rahulsharma312004@gmail.com>
2025-04-11 20:08:14 +0530

    expiring token

--- src/app.js
+++ src/app.js
@@ -7,27 +7,11 @@ const signupValidation = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const validator = require("validator");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.email;
-
-  try {
-    console.log(userEmail);
-    const users = await User.find({ email: userEmail });
-
-    if (users.length === 0) {
-      res.status(400).send("user getting error:" + err.message);
-    } else {
-      res.send(users);
-    }
-  } catch (err) {
-    res.status(400).send("user getting error: " + err.message);
-  }
-});
 
 app.post("/signup", async (req, res) => {
   try {
@@ -39,7 +23,7 @@ app.post("/signup", async (req, res) => {
 
     // console.log(hashPassword);
     const user = new User({
-      firstName, 
+      firstName,
       lastName,
       email,
       password: hashPassword,
@@ -55,19 +39,20 @@ app.post("/login", async (req, res) => {
   try {
     //validate
     const { email, password } = req.body;
+    if (!validator.isEmail(email)) throw new Error("insert correct email");
     const user = await User.findOne({ email: email });
     if (!user) {
-      throw new Error("email not registered");
+      throw new Error("Invalid Credentials");
     }
     const comparePassword = await bcrypt.compare(password, user.password);
     if (comparePassword) {
       //token generate
-      const token = await jwt.sign({ _id: user._id }, "Devconnect");
+      const token = await jwt.sign({ _id: user._id }, "Devconnect",{expiresIn:'14d'});
       //send in cookie
       res.cookie("token", token);
       res.send("login succesfull");
     } else {
-      res.status(400).send("invalid password");
+      res.status(400).send("Invalid Credentials");
     }
   } catch (err) {
     res.status(400).send("Error in loging in:" + err.message);
@@ -82,30 +67,12 @@ app.get("/profile", userAuthentication, async (req, res) => {
     res.status(400).send("Error getting profile" + err.message);
   }
 });
-app.patch("/user/:userid", async (req, res) => {
-  const userId = req.params?.userid;
-  const data = req.body;
-  try {
-    const Update_Allowed = ["skills", "age", "gender"];
-    const user_allowwed = Object.keys(data).every((k) =>
-      Update_Allowed.includes(k)
-    );
-    if (!user_allowwed) {
-      throw new Error("update dont allowed");
-    }
-    if (data?.skills.length > 10) {
-      throw new Error("skills dont allowed");
-    }
-    const user = await User.findOneAndUpdate({ _id: userId }, data, {
-      runValidators: true,
-      returnDocument: "after",
-    });
-    res.send("updated succesfully");
-    console.log(user);
-  } catch (err) {
-    res.status(400).send("update failed :" + err.message);
-  }
-});
+
+app.post("/sendConnectionRequest",userAuthentication,async(req,res)=>{
+      user= req.user;
+            res.send(user.firstName+" send the connection request");
+            console.log("connection req sent");
+})
 
 connectDb()
   .then(() => {
